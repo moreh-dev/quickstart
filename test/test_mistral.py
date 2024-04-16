f0bd3086c9c988bae67b3fa63ab1bb0a80c93d63
안재학 <jeight@anjaehag-ui-MacBookPro.local>
2024-04-16 15:05:10 +0900

    [MOD] change test prompt

--- test/test_mistral.py
+++ test/test_mistral.py
@@ -1,4 +1,6 @@
 import torch
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]), 'model'))
 
 from transformers import AutoTokenizer
 from modeling_mistral import MistralForCausalLM
@@ -11,7 +13,7 @@ CHECKPOINT = "./code_generation"
 MODEL_NAME = "mistralai/Mistral-7B-v0.1"
 
 # Max New Tokens for generating
-MAX_NEW_TOKENS = 1024
+MAX_NEW_TOKENS = 512
 
 # Load trained model
 model = MistralForCausalLM.from_pretrained(CHECKPOINT)
@@ -20,13 +22,14 @@ model.cuda()
 model.eval()
 
 # Prepare test prompt
-input_text = f"[INST] Write a python function to find the volume of a triangular prism. [/INST]"
+input_text = f"[INST] Write a function to sort a given matrix in ascending order according to the sum of its rows. [/INST]"
 input_ids = tokenizer(input_text, return_tensors="pt").input_ids
 generated_text_ids = input_ids.cuda()
 
-# Generate python function
-output = model.generate(generated_text_ids, max_new_tokens=MAX_NEW_TOKENS)
+with torch.no_grad():
+    # Generate python function
+    output = model.generate(generated_text_ids, max_new_tokens=MAX_NEW_TOKENS)
 
-# Decode generated tokens
-generated_text = tokenizer.decode(output, skip_special_tokens=True)
-print(f"Mistral: {generated_text}")
+    # Decode generated tokens
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    print(f"Mistral: {generated_text}")
