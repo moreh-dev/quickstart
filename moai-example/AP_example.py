4287d3c627a76d77e53cbf0517697ac959c2ea5e
Gwangho Choi <gwangho.choi@moreh.io>
2024-11-11 18:28:29 +0900

    Update AP_example.py

--- moai-example/AP_example.py
+++ moai-example/AP_example.py
@@ -31,7 +31,7 @@ def parse_args():
     parser.add_argument(
         "--model-name-or-path",
         type=str,
-        default="./llama-2-13b-hf",
+        default="meta-llama/Llama-2-13b",
         help="model name or path",
     )
     parser.add_argument(
